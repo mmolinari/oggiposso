44dd1d8b88b6db4bd1aff332bb0a4ffd2a0ce255
Marco Molinari <marco.molinari@gmail.co>
2021-01-05 11:32:33 +0100

    Removed old ununsed file.

--- src/labels/zones.js
+++
@@ -1,9 +0,0 @@
-// Map keys to labels.
-export const Zones = {
-  "yellow": "giallo",
-  "yellow_no_regions_school": "giallo, senza spostamenti tra regioni, superiori in presenza al 50%",
-  "yellow_no_regions_no_school": "giallo, senza spostamenti tra regioni, superiori a distanza",
-  "orange": "arancione",
-  "red": "rosso",
-  "red_christmas": "rosso, con eccezioni",
-};
