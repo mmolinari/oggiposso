44af1d5a54446f22d90aeb912499705143d3a469
Marco Molinari <marco.molinari@gmail.co>
2021-01-09 02:15:44 +0100

    Added dark mode.

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,23 +7,18 @@ import SEO from "./seo";
 const Layout = ( { location, children } ) => {
   const rootPath = `${ __PATH_PREFIX__ }/`
   const isRootPath = location.pathname === rootPath
-  const title = "Oggi Posso";
+  const [mode, setMode] = React.useState(localStorage.getItem('theme') ?? 'light');
 
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1>
-        <Link to="/">{ title }</Link>
-      </h1>
-    )
-  }
-  else {
-    header = (
-      <Link to="/">
-        { title }
-      </Link>
-    )
+  function switchMode () {
+    let newMode;
+    if (mode === 'light') {
+      newMode = 'dark';
+    }
+    else {
+      newMode = 'light';
+    }
+    localStorage.setItem('theme', newMode);
+    setMode(newMode);
   }
 
   function separator() {
@@ -32,51 +27,55 @@ const Layout = ( { location, children } ) => {
     );
   }
 
-  // @todo: display site name?
-
   return (
-    <div className="container px-4 md:px-8 lg:px-12 font-sans text-gray-900" data-is-root-path={ isRootPath }>
-      <SEO/>
-      <header/>
-      <main>
-        { children }
-      </main>
-      <footer>
-        <div className="text-center">
+    <div className={mode === 'dark' ? 'dark' : ''}>
+      <div className="bg-gray-50 text-gray-900 dark:bg-gray-800 dark:text-gray-300">
+        <div className="container px-4 md:px-8 lg:px-12 font-sans" data-is-root-path={ isRootPath }>
+          <SEO/>
+          <header/>
+          <main>
+            { children }
+          </main>
+          <footer>
+            <div className="text-center">
           <span className="underline">
             <Link to="/">Home</Link>
           </span>
-          { separator() }
-          <span className="underline">
+              { separator() }
+              <span className="underline">
             <Link to="/terms">Termini di utilizzo</Link>
           </span>
-          { separator() }
-          <span className="underline">
+              { separator() }
+              <span className="underline">
             <Link to="/privacy">Informativa sulla privacy</Link>
           </span>
-          { separator() }
-          <span className="underline">
+              { separator() }
+              <span className="underline">
             <a href="https://docs.google.com/forms/d/e/1FAIpQLSfiuthLNYyeeOd1n5J0dHclP-3mrRvv8K4lQGzkBlGem8-nzA/viewform">Contatti</a>
           </span>
-          { separator() }
-          <span className="underline">
+              { separator() }
+              <span className="underline">
             <a href="https://github.com/mmolinari/oggiposso">GitHub</a>
           </span>
-        </div>
+            </div>
 
-        <div className="text-center">
-          <LastUpdate></LastUpdate>
-        </div>
+            <div className="text-center">
+              <LastUpdate></LastUpdate>
+            </div>
 
-        <div className="text-xs text-center">
-          Icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a
-          href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-        </div>
+            <div className="text-xs text-center">
+              Icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a
+              href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+            </div>
 
-        <div className="m-8">
-          <Sources></Sources>
+            <div className="p-8">
+              <Sources></Sources>
+            </div>
+            <span onClick={switchMode} className="text-xs fixed bottom-3 right-3 cursor-pointer">{mode === 'light' ? '🌑' : '☀️'}</span>
+
+          </footer>
         </div>
-      </footer>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // false, 'media' or 'class'
   theme: {
     extend: {},
     colors: {
+      gray: colors.trueGray,
       yellow: colors.amber,
       orange: colors.orange,
       red: colors.red,
